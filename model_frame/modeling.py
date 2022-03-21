837454a0bb53b2a3fa2f6628102611e358f5b6b8
yangdongquan <739260804@qq.com>
2022-03-21 14:33:20 +0800

    model forward success

--- model_frame/modeling.py
+++ model_frame/modeling.py
@@ -1,9 +1,7 @@
 import torch
 import torchvision.models as models
 import torch.nn as nn
-import cv2
-from transformers import BertTokenizer
-
+from data_process.build_dataloader import get_ima_txt_data_loader
 import config as cfg
 
 
@@ -14,10 +12,19 @@ class ImageEmbedding(nn.Module):
         self.resnet18 = models.resnet18(pretrained=True)
         self.linear = nn.Linear(1000, 768)
 
-    # img_tensor.shape = batch*48*48*3
+    # img_tensor.shape = batch*432(图片拆分块数)*48*48*3
+    # return batch*432(图片拆分块数)*768
     def forward(self, img_tensor):
-        img_tensor.transpose_(1, 3)
-        patch_ebds = self.resnet18(img_tensor)
+        img_tensor = img_tensor.permute(0, 1, 4, 2, 3)
+        batch_len_ebd = torch.zeros((cfg.BATCH_SIZE, 432, 768))
+        for i, p in enumerate(img_tensor):
+            ebd = self.deal_one_page(p)
+            batch_len_ebd[i] = ebd
+
+        return batch_len_ebd
+
+    def deal_one_page(self, patch):
+        patch_ebds = self.resnet18(patch)
         patch_ebds2 = self.linear(patch_ebds)
 
         pos_ebd = self.pos_embedding(torch.arange(432))
@@ -31,18 +38,36 @@ class TextEmbedding(nn.Module):
         ebd_dict = sd['bert.embeddings.word_embeddings.weight']
         self.text_embedding = text_embedding = nn.Embedding.from_pretrained(ebd_dict, freeze=False)
 
-    def forward(self, tokenids):
-        token_ebd = self.text_embedding(tokenids)
+    # token_ids.shape = batch*len
+    # return batch*len*768
+    def forward(self, token_ids):
+        token_ebd = self.text_embedding(token_ids)
         return token_ebd
 
 
 class TransformerModel(nn.Module):
     def __init__(self):
         super(TransformerModel, self).__init__()
+        self.img_embedding = ImageEmbedding()
+        self.text_embedding = TextEmbedding()
+        self.tf = nn.Transformer(d_model=768)
 
-    def forward(self):
-        pass
+    def forward(self, patch, token_ids):
+        img_ebd = self.img_embedding(patch)
+        txt_ebd = self.text_embedding(token_ids)
+        # batch 和 len 交换维度
+        img_ebd = img_ebd.transpose(0, 1)
+        txt_ebd = txt_ebd.transpose(0, 1)
+        print(txt_ebd.shape, img_ebd.shape)
+        out = self.tf(img_ebd, txt_ebd)
+        return out
 
 
 if __name__ == '__main__':
-    transformer_model = nn.Transformer(d_model=768)
+    transformer_model = TransformerModel()
+
+    dl = get_ima_txt_data_loader()
+    for batch_patch, batch_tokens, batch_labels in dl:
+        out = transformer_model(batch_patch, batch_tokens)
+        print(out.shape)
+        break
